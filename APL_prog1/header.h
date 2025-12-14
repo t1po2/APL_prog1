8757e58d7b2e93899d303400c04cc51658e23074
Hung nguyen <vhung.nguyen05@gmail.com>
2025-12-15 00:29:39 +0100

    Basis

---
+++ APL_prog1/header.h
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+typedef struct {
+	int id;
+	char name[30];
+	char name[30];
+	int average;
+}t_schueler;
+
+
+typedef struct {
+	int id;
+	int fach1;
+	int fach2;
+	int fach3;
+	int fach4;
+	int fach5;
+	
+}t_noten;
